c28ff93b67a3da5cbff0d2ffeddfc16fd1dd3438
Haishan <haishanhan@gmail.com>
2019-11-17 17:31:59 +0800

    refactor: improve UI for small screens

--- webpack.config.js
+++ webpack.config.js
@@ -101,6 +101,18 @@ const loaders = {
       plugins: () =>
         [
           require('postcss-import')(),
+          require('postcss-custom-media')({
+            importFrom: [
+              {
+                customMedia: {
+                  '--breakpoint-not-small': 'screen and (min-width: 30em)',
+                  '--breakpoint-medium':
+                    'screen and (min-width: 30em) and (max-width: 60em)',
+                  '--breakpoint-large': 'screen and (min-width: 60em)'
+                }
+              }
+            ]
+          }),
           require('postcss-nested')(),
           require('autoprefixer')(),
           require('postcss-extend-rule')(),
