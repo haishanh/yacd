361effbd9864f8da3456fcd06094c30919fd822e
Haishan <haishanhan@gmail.com>
2020-06-07 18:38:27 +0800

    changed: switch primary font family from Merriweather Sans to Inter
    also starting to self hosting font files

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 import Root from './components/Root';
 
+// eslint-disable-next-line no-unused-expressions
+import('typeface-inter');
+// eslint-disable-next-line no-unused-expressions
+import('typeface-roboto-mono');
+
 const rootEl = document.getElementById('app');
 
 Modal.setAppElement(rootEl);
