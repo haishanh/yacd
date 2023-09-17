9bbd434f647b0a33e6524af7177cfd51317729a5
haishan <haishanhan@gmail.com>
2023-09-17 19:06:06 +0800

    Upgrade react-table

--- src/components/ConnectionTable.tsx
+++ src/components/ConnectionTable.tsx
@@ -1,121 +1,142 @@
+import {
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from '@tanstack/react-table';
 import cx from 'clsx';
 import { formatDistance } from 'date-fns';
 import React from 'react';
 import { ChevronDown } from 'react-feather';
-import { useSortBy, useTable } from 'react-table';
 
 import prettyBytes from '../misc/pretty-bytes';
 import s from './ConnectionTable.module.scss';
 import { MutableConnRefCtx } from './conns/ConnCtx';
 
-const sortDescFirst = true;
-
 const fullColumns = [
-  { accessor: 'id', show: false },
-  { Header: 'Host', accessor: 'host' },
-  { Header: 'Process', accessor: 'process' },
-  { Header: 'DL', accessor: 'download', sortDescFirst },
-  { Header: 'UL', accessor: 'upload', sortDescFirst },
-  { Header: 'DL Speed', accessor: 'downloadSpeedCurr', sortDescFirst },
-  { Header: 'UL Speed', accessor: 'uploadSpeedCurr', sortDescFirst },
-  { Header: 'Chains', accessor: 'chains' },
-  { Header: 'Rule', accessor: 'rule' },
-  { Header: 'Time', accessor: 'start', sortDescFirst },
-  { Header: 'Source', accessor: 'source' },
-  { Header: 'Destination IP', accessor: 'destinationIP' },
-  { Header: 'Type', accessor: 'type' },
+  { header: 'Id', accessorKey: 'id' },
+  { header: 'Host', accessorKey: 'host' },
+  { header: 'Process', accessorKey: 'process' },
+  {
+    header: 'DL', accessorKey: 'download',
+    cell: (info: any) => prettyBytes(info.getValue())
+  },
+  {
+    header: 'UL', accessorKey: 'upload',
+    cell: (info: any) => prettyBytes(info.getValue())
+  },
+  {
+    header: 'DL Speed', accessorKey: 'downloadSpeedCurr',
+    cell: (info: any) => prettyBytes(info.getValue()) + '/s'
+  },
+  {
+    header: 'UL Speed', accessorKey: 'uploadSpeedCurr',
+    cell: (info: any) => prettyBytes(info.getValue()) + '/s'
+  },
+  { header: 'Chains', accessorKey: 'chains' },
+  { header: 'Rule', accessorKey: 'rule' },
+  {
+    header: 'Time', accessorKey: 'start',
+    cell: (info: any) => formatDistance(info.getValue(), 0)
+  },
+  { header: 'Source', accessorKey: 'source' },
+  { header: 'Destination IP', accessorKey: 'destinationIP' },
+  { header: 'Type', accessorKey: 'type' },
 ];
 
+const COLUMN_SORT = [
+  { id: 'id', desc: true }
+]
+
 const columns = fullColumns;
-const columnsWithoutProcess = fullColumns.filter((item) => item.accessor !== 'process');
-
-function renderCell(cell: { column: { id: string }; value: number }) {
-  switch (cell.column.id) {
-    case 'start':
-      return formatDistance(cell.value, 0);
-    case 'download':
-    case 'upload':
-      return prettyBytes(cell.value);
-    case 'downloadSpeedCurr':
-    case 'uploadSpeedCurr':
-      return prettyBytes(cell.value) + '/s';
-    default:
-      return cell.value;
-  }
-}
-
-const sortById = { id: 'id', desc: true };
-const tableState = {
-  sortBy: [
-    // maintain a more stable order
-    sortById,
-  ],
-  hiddenColumns: ['id'],
-};
+const columnsWithoutProcess = fullColumns.filter((item) => item.accessorKey !== 'process');
 
 function Table({ data }: { data: any }) {
   const connCtx = React.useContext(MutableConnRefCtx);
-  const { getTableProps, headerGroups, rows, prepareRow } = useTable(
+  const [sorting, setSorting] = React.useState<SortingState>(COLUMN_SORT)
+  const table = useReactTable(
     {
       columns: connCtx.hasProcessPath ? columns : columnsWithoutProcess,
       data,
-      initialState: tableState,
-      autoResetSortBy: false,
+      state: {
+        sorting,
+        columnVisibility: { id: false }
+      },
+      onSortingChange: setSorting,
+      getCoreRowModel: getCoreRowModel(),
+      getSortedRowModel: getSortedRowModel(),
+      // debugTable: true,
     },
-    useSortBy
   );
+  // className={s.tr}
   return (
-    <div
-      {...getTableProps()}
-      style={{
-        // @ts-ignore
-        '--col-count': connCtx.hasProcessPath ? '12' : '11',
-      }}
-    >
-      {headerGroups.map((headerGroup, i) => {
-        return (
-          <div key={i} {...headerGroup.getHeaderGroupProps()} className={s.tr}>
-            {headerGroup.headers.map((column, idx) => (
-              <div key={idx} {...column.getHeaderProps(column.getSortByToggleProps())} className={s.th}>
-                <span>{column.render('Header')}</span>
-                <span className={s.sortIconContainer}>
-                  {column.isSorted ? (
-                    <span className={column.isSortedDesc ? '' : s.rotate180}>
-                      <ChevronDown size={16} />
-                    </span>
-                  ) : null}
-                </span>
-              </div>
-            ))}
-
-            {rows.map((row, i) => {
-              prepareRow(row);
-              return row.cells.map((cell, j) => {
+    <table className={s.table}>
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => {
+          return (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
                 return (
-                  <div
-                    key={j}
-                    {...cell.getCellProps()}
-                    className={cx(
-                      s.td,
-                      i % 2 === 0 ? s.odd : false,
-                      connCtx.hasProcessPath
-                        ? j >= 2 && j <= 5
-                          ? s.du
-                          : false
-                        : j >= 1 && j <= 4
-                          ? s.du
-                          : false
-                    )}
-                  >
-                    {renderCell(cell)}
-                  </div>
-                );
-              });
-            })}
-          </div>
-        );
-      })}
-    </div>
+                  <th key={header.id}
+                    className={header.column.getCanSort() ? cx(s.th, s.pointer) : s.th}
+                    onClick={header.column.getToggleSortingHandler()}>
+                    <span className={s.thWrap}>
+                      <span>
+                        {
+                          flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )
+                        }
+                      </span>
+                      {header.column.getIsSorted() ? (
+                        <span className={header.column.getIsSorted() === 'desc' ? s.sortIconContainer : cx(s.rotate180, s.sortIconContainer)}>
+                          <ChevronDown size={16} />
+                        </span>
+                      ) : null}
+                    </span>
+                  </th>
+                )
+              })}
+            </tr>)
+        })}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => {
+          return (
+            <tr key={row.id}>
+              {
+                row.getVisibleCells().map((cell) => {
+                  return (
+                    <td
+                      key={cell.id}
+                      className={cx(
+                        'xxxx'
+                        // s.td,
+                        // i % 2 === 0 ? s.odd : false,
+                        // connCtx.hasProcessPath
+                        //   ? j >= 2 && j <= 5
+                        //     ? s.du
+                        //     : false
+                        //   : j >= 1 && j <= 4
+                        //     ? s.du
+                        //     : false
+                      )}
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  );
+                })
+              }
+            </tr>
+          )
+        })}
+      </tbody>
+    </table >
   );
 }
 
